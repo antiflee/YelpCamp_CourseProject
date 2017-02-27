594ab4553b834ad4da75da5db4c68fa54a870cf4
YFL <antiflee@gmail.com>
2017-02-27 07:13:12 +0000

    add mongolab

--- app.js
+++ app.js
@@ -16,7 +16,9 @@ var commentRoutes       = require("./routes/comments");
 var campgroundRoutes    = require("./routes/campgrounds");
 var indexRoutes         = require("./routes/index");
 
-mongoose.connect("mongodb://localhost/yelp_camp");
+//mongoose.connect("mongodb://localhost/yelp_camp");
+mongoose.connect("mongodb://antiflee:lyf5329987@ds029735.mlab.com:29735/yelpcamp");
+// mongodb://<antiflee>:<lyf5329987>@ds029735.mlab.com:29735/yelpcamp
 app.use(bodyParser.urlencoded({extended:true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname+"/public"));
